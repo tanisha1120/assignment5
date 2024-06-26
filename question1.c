2ae98693bd25528c41afc6b179b8151989762f90
tanisha1120 <tanishasingh.cse23@kgikanpur.in>
2024-06-26 15:17:49 +0530

    Update question1.c

--- question1.c
+++ question1.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main(){
-    int number,i ;
+    int number,i =1;
     printf("ENter the number ");
     scanf("%d",&number);
     
     while(number >= i){
-        printf("%d ",i);
+        printf("%d \n",i);
         i++;
     }
 

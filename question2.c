210c626bd467a232cd620b47dcdeb436792c1974
tanisha1120 <tanishasingh.cse23@kgikanpur.in>
2024-06-26 15:16:10 +0530

    Create question2.c

---
+++ question2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int number;
+    printf("ENter the number ");
+    scanf("%d",&number);
+    
+    while(number >=1){
+        printf("%d \n",number);
+        n--;
+}
+
+}

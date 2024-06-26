691452faf787d9ba430a6caf3b538aa129930383
tanisha1120 <tanishasingh.cse23@kgikanpur.in>
2024-06-26 15:41:45 +0530

    Create ques7.c

---
+++ ques7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+  int n,i,sum; 
+  printf("Enter the number ");
+  scanf("%d",&n);
+   
+  while(i<=n){
+      if(i%2 ==0){
+          sum = sum +i;
+      }
+      i++;
+      
+  }
+ 
+  printf("The sum of the numbers is %d",sum);
+
+}
